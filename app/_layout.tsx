58e24b369dbc3902f9179f9ce491e4310ab4ad28
juliacapereira <carrielj23@outlook.com>
2026-04-06 15:32:44 -0300

    barra de navegação superior ajustada para pagina login e cadastros, cadastros so da msg que deu sucesso se preencher todos os espaços

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,7 +14,15 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#ffffff',
+          },
+          headerTintColor: '#000000',
+          headerTitle: '', 
+        }}
+      >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>

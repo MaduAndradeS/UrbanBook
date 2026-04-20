7a1568c5904c002b5c1d356c931d472926578488
juliacapereira <carrielj23@outlook.com>
2026-04-19 22:52:25 -0300

    paginas de teste para adm aprovar empresario criadas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const clienteRoutes = require('./cliente.routes');
 const empresarioRoutes = require('./empresario.routes');
 const authRoutes = require('./auth.routes');
+const admRoutes = require('./adm.routes');
 
 router.get('/', (req, res) => {
   res.send('API UrbanBook funcionando');
@@ -12,5 +13,8 @@ router.get('/', (req, res) => {
 router.use('/clientes', clienteRoutes);
 router.use('/empresarios', empresarioRoutes);
 router.use('/auth', authRoutes);
+
+// ROTAS TEMPORÁRIAS DE TESTE PARA ADM
+router.use('/test/adms', admRoutes);
 
 module.exports = router;
